a83e1dc7a2e084c520d93dfdc0f4cf236f8f6d29
Thomas Gusewelle <thomasgusewelle@pop-os.localdomain>
2023-09-26 09:42:25 -0500

    added neo tree offset

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -5,8 +5,18 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     opts = {
       options = {
-        always_show_bufferline = false
+        always_show_bufferline = false,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-Tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+
       }
+
     },
   }
 }
