b4b04d8628f2ed2f7b403468dc25f694c36695b7
Thomas Gusewelle <thomasgusewelle21@gmail.com>
2023-10-24 20:10:40 -0500

    add formating command for flutter

--- lua/custom/plugins/flutter.lua
+++ lua/custom/plugins/flutter.lua
@@ -11,6 +11,7 @@ return {
       {"<leader>fr", "<cmd>FlutterRun<cr>", "Flutter Run"},
       { "<leader>fd", "<cmd>FlutterDevices<cr>", "Flutter Devices"},
       {"<leader>fe", "<cmd>FlutterEmulators<cr>", "Flutter Emulators"},
+      {"<leader>ff", "<cmd>lua vim.lsp.buf.format()>", "Flutter Format"},
       {"<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Actions"},
       {"K", "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover"}
     }
