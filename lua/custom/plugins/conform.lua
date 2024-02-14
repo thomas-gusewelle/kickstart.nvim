e18edeebccecad9e73fd73659a725a081f2d71d4
Thomas <tgusewelle@gkwmedia.com>
2024-02-14 18:41:46 +0000

    switch to conform from null-ls

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,23 +1,36 @@
 return {
   'stevearc/conform.nvim',
-  opts = {
-    formatters_by_ft = {
-      ["javascript"] = { { "prettierd", "prettier" } },
-      ["javascriptreact"] = { { "prettierd", "prettier" } },
-      ["typescript"] = { { "prettierd", "prettier" } },
-      ["typescriptreact"] = { { "prettierd", "prettier" } },
-      ["vue"] = { { "prettierd", "prettier" } },
-      ["css"] = { { "prettierd", "prettier" } },
-      ["scss"] = { { "prettierd", "prettier" } },
-      ["less"] = { { "prettierd", "prettier" } },
-      ["html"] = { { "prettierd", "prettier" } },
-      ["json"] = { { "prettierd", "prettier" } },
-      ["jsonc"] = { { "prettierd", "prettier" } },
-      ["yaml"] = { { "prettierd", "prettier" } },
-      ["markdown"] = { { "prettierd", "prettier" } },
-      ["markdown.mdx"] = { { "prettierd", "prettier" } },
-      ["graphql"] = { { "prettierd", "prettier" } },
-      ["handlebars"] = { { "prettierd", "prettier" } },
-    },
-  },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require('conform')
+    conform.setup({
+      formatters_by_ft = {
+        ["javascript"] = { { "prettierd", "prettier" } },
+        ["javascriptreact"] = { { "prettier", "prettier" } },
+        ["typescript"] = { { "prettierd", "prettier" } },
+        ["typescriptreact"] = { { "prettierd", "prettier" } },
+        ["vue"] = { { "prettierd", "prettier" } },
+        ["css"] = { { "prettierd", "prettier" } },
+        ["scss"] = { { "prettierd", "prettier" } },
+        ["less"] = { { "prettierd", "prettier" } },
+        ["html"] = { { "prettierd", "prettier" } },
+        ["json"] = { { "prettierd", "prettier" } },
+        ["jsonc"] = { { "prettierd", "prettier" } },
+        ["yaml"] = { { "prettierd", "prettier" } },
+        ["markdown"] = { { "prettierd", "prettier" } },
+        ["markdown.mdx"] = { { "prettierd", "prettier" } },
+        ["graphql"] = { { "prettierd", "prettier" } },
+        ["handlebars"] = { { "prettierd", "prettier" } },
+      },
+    })
+
+    vim.keymap.set({ "n", 'v' }, "<leader>cf", function()
+      conform.format({
+        lsp_fallback = true,
+        async = true,
+        timeout_ms = 500,
+      })
+    end, { desc = "Code Format Test" }
+    )
+  end,
 }
