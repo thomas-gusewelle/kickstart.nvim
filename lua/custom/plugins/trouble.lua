38577fd32270e7a982f3f9d8f9c23af3e9ad979e
Thomas <tgusewelle@gkwmedia.com>
2024-03-13 20:37:15 -0500

    trouble desc

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,9 +1,13 @@
 return {
- "folke/trouble.nvim",
- dependencies = { "nvim-tree/nvim-web-devicons" },
- opts = {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
-},
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("trouble").setup()
+    vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end, {desc = "Trouble Toggle"})
+    vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end, {desc = "Workspace Diagnostics"})
+    vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end, {desc = "Document Diapnostics"})
+    vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end, {desc = "Quckfix"})
+    vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end, {desc = "Loclist"})
+    vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end, {desc = "Lsp References"})
+  end
 }
